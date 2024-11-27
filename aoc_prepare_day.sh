7b54793cf2c593152b3d82c3fda36628a9810077
Johan Svanström <svanis71@gmail.com>
2024-11-27 21:05:06 +0100

    aoc template

--- aoc_prepare_day.sh
+++ aoc_prepare_day.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+if [ ! -e "main.py" ]; then
+    echo "from time import perf_counter
+
+# insert import before (don't remove this line)
+
+runall = True
+if __name__ == '__main__':
+    if runall:
+
+" > main.py
+
+fi
+
 day=$1 
 mkdir -p day${day} 
 echo "from indata import read_list_of_strings
@@ -33,8 +46,7 @@ echo "from .day${day} import run" > day${day}/__init__.py
 touch indata/day${day}.txt
 touch indata/day${day}_test.txt
 
-sed "/^\# insert import before/i import day${day}" main.py > main_tmp.py
-mv main_tmp.py main.py
+sed -i "/^\# insert import before/i import day${day}" main.py
 
 echo "        day${day}.run()
         print(f'Time: {perf_counter() - start} seconds')
